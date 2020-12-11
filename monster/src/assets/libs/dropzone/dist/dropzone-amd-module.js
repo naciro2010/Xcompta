0b358d6ee181877f2ce4b6979a1bd60808197059
MohamedEnnaciri <extern.mohamed.ennaciri@europcar.com>
2020-12-11 19:54:47 +0100

    add creation marché

--- monster/src/assets/libs/dropzone/dist/dropzone-amd-module.js
+++ monster/src/assets/libs/dropzone/dist/dropzone-amd-module.js
@@ -433,7 +433,7 @@ var Dropzone = function (_Emitter) {
         /**
          * The text used before any files are dropped.
          */
-        dictDefaultMessage: "Drop files here to upload",
+        dictDefaultMessage: "Joindre des fichiers ",
 
         /**
          * The text that replaces the default message text it the browser is not supported.
